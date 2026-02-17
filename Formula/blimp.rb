6e65d67ac2b9f76b4aaf32100d8a26cad6f4d081
Max K. <31866271+havebeenfitz@users.noreply.github.com>
2026-02-17 14:04:46 +0100

    Update blimp to 0.7.1

--- Formula/blimp.rb
+++ Formula/blimp.rb
@@ -1,13 +1,13 @@
 class Blimp < Formula
   desc "Modern Swift CLI for iOS/macOS app deployment to TestFlight/App Store"
   homepage "https://github.com/platacard/blimp"
-  version "0.7.0"
+  version "0.7.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/platacard/blimp/releases/download/0.7.0/blimp-0.7.0-macos-arm64.tar.gz"
-      sha256 "b8b8f3a739744ba57fc9a3ac4ef22c5cb804d6bee3d95f62360416ca3a979f3c"
+      url "https://github.com/platacard/blimp/releases/download/0.7.1/blimp-0.7.1-macos-arm64.tar.gz"
+      sha256 "5ddcb8fd8ae05fb2f16d7b40b71cecc2e79473a746f792097aa2dcd056665b6f"
     end
   end
 
